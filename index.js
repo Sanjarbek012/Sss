ad642b158e2268c8a7958ddf19ff825cbd662fe8
Sanjarbek012 <sanjarbekxaytboyevv4@gmail.com>
2026-04-08 13:45:12 +0500

    init

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+       // amaliyot 
 // 1-masala 
 
 // function minimumTotal(triangle) {
@@ -80,4 +81,154 @@
 // }
 
 // const arr4 = [1, null, "hello", true, {}, []];
-// console.log("4:", getTypes(arr4))
+// console.log("4:", getTypes(arr4)) 
+
+
+//   Uyga vazifa  
+
+// 1-masala  
+
+// function findClosestNeighbours(arr, R) {
+//   let closestPair = [];
+//   let minDiff = Infinity;
+
+//   for (let i = 0; i < arr.length - 1; i++) {
+//     let sum = arr[i] + arr[i + 1];
+//     let diff = Math.abs(R - sum);
+
+//     if (diff < minDiff) {
+//       minDiff = diff;
+//       closestPair = [arr[i], arr[i + 1]];
+//     }
+//   }
+
+//   return closestPair;
+// }
+
+// console.log(findClosestNeighbours([21, 43, 11, 3, 4, 32, 54, 31, 35], 35)); 
+ 
+
+// 2-masala  
+
+// function replaceNextEven(arr) {
+//   let lastEven = null;
+
+//   return arr.map(num => {
+//     if (num % 2 === 0) {
+//       if (lastEven !== null) {
+//         let temp = lastEven;
+//         lastEven = num;
+//         return num;
+//       }
+//       lastEven = num;
+//     }
+//     return num;
+//   });
+// }
+
+// console.log(replaceNextEven([1, 4, 5, 2, 33, 1, 8, 5, 6, 33, 1, 12, 26]));  
+
+
+// 3-masala 
+
+// function replaceWithLastOdd(arr) {
+//   let lastOdd = arr.filter(x => x % 2 !== 0).slice(-1)[0];
+
+//   return arr.map(num => num % 2 !== 0 ? lastOdd : num);
+// }
+
+// console.log(replaceWithLastOdd([2, 55, 2, 21, 44, 3])); 
+
+// 4-masala 
+
+// function swapMinMax(arr) {
+//   let min = Math.min(...arr);
+//   let max = Math.max(...arr);
+
+//   return arr.map(x => {
+//     if (x === min) return max;
+//     if (x === max) return min;
+//     return x;
+//   });
+// }
+
+// console.log(swapMinMax([23, 43, 12, 56, 64]));
+
+// 5-masala 
+
+// function zeroBetweenMinMax(arr) {
+//   let minIndex = arr.indexOf(Math.min(...arr));
+//   let maxIndex = arr.indexOf(Math.max(...arr));
+
+//   let start = Math.min(minIndex, maxIndex);
+//   let end = Math.max(minIndex, maxIndex);
+
+//   return arr.map((x, i) => (i > start && i < end ? 0 : x));
+// }
+
+// console.log(zeroBetweenMinMax([2, 54, 4, 11, 1, 34, 11]));
+
+// 6-masala  
+
+// function reverseBetweenMinMax(arr) {
+//   let minIndex = arr.indexOf(Math.min(...arr));
+//   let maxIndex = arr.indexOf(Math.max(...arr));
+
+//   let start = Math.min(minIndex, maxIndex);
+//   let end = Math.max(minIndex, maxIndex);
+
+//   let middle = arr.slice(start + 1, end).reverse();
+
+//   return [
+//     ...arr.slice(0, start + 1),
+//     ...middle,
+//     ...arr.slice(end)
+//   ];
+// }
+
+// console.log(reverseBetweenMinMax([2, 54, 4, 11, 1, 34, 11]));
+
+// 7-masala  
+
+// function removeNumber(arr, k) {
+//   return arr.filter((_, index) => index !== k);
+// }
+
+// console.log(removeNumber([54, 23, 65, 12], 1)); 
+
+
+// 8-masala  
+
+// function removeDuplicate(arr) {
+//   return [...new Set(arr)];
+// }
+
+// console.log(removeDuplicate([12, 43, 23, 54, 12])); 
+
+// 9-masala  
+
+// function threeElements(arr) {
+//   let count = {};
+
+//   arr.forEach(x => count[x] = (count[x] || 0) + 1);
+
+//   return arr.filter(x => count[x] >= 3);
+// }
+
+// console.log(threeElements([12, 12, 44, 54, 44, 44, 2, 22, 2, 2])); 
+
+// 10-masala  
+
+// function arrayIndexSum(arr) {
+//   let sum = 0;
+
+//   arr.forEach((num, i) => {
+//     if (num % 2 === 0) {
+//       sum += i;
+//     }
+//   });
+
+//   return sum;
+// }
+
+// console.log(arrayIndexSum([2, 43, 53, 23, 22, 53, 88, 10])); 
